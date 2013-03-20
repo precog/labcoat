2536b0d93780eb63fa2456d099f87f5a49a2074f
Brian McKenna <brian@precog.com>
2013-03-20 14:06:58 -0600

    Export the Labcoat git version to window.labcoat2.version

--- src/precog/app/Main.hx
+++ src/precog/app/Main.hx
@@ -3,13 +3,18 @@ package precog.app;
 import precog.communicator.ModuleManager;
 import js.JQuery;
 import precog.app.module.view.*;
+import js.Browser.window;
 
 class Main 
 {
-	static function main()
-	{
-		var manager = new ModuleManager();
-		manager.addModule(new ContainerModule());
-		manager.addModule(new LayoutModule());
-	}
+    static function main()
+    {
+        // Add the Labcoat git version to the window.
+        Reflect.setField(window, "labcoat2", {});
+        Reflect.setField(untyped window.labcoat2, "version", VersionMacro.gitVersion());
+
+        var manager = new ModuleManager();
+        manager.addModule(new ContainerModule());
+        manager.addModule(new LayoutModule());
+    }
 }
---
+++ src/precog/app/VersionMacro.hx
@@ -0,0 +1,14 @@
+package precog.app;
+
+import haxe.macro.Context;
+
+using StringTools;
+
+class VersionMacro {
+    // Must be executed from a cloned Labcoat repository.
+    public static macro function gitVersion() {
+        var gitProcess = new sys.io.Process('git', ['describe', '--always', '--dirty', '--long', '--tags']);
+        var gitDescription = gitProcess.stdout.readAll().toString().trim();
+        return Context.makeExpr(gitDescription, Context.currentPos());
+    }
+}
