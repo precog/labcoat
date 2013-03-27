1079049cf9c83446bc002371abee10b5295d1583
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-03-27 08:52:12 -0600

    renamed localization to translation

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,4 @@
 clear
 neko livehaxe.n -haxe debug.html5.hxml\
 	-less css/labcoat.less bin/debug/html5/css/labcoat.css -haxe-port 7777\
-	-po2json localization/po/it-it.po bin/debug/html5/translation/it-it.json
+	-po2json translation/po/it-it.po bin/debug/html5/translation/it-it.json
