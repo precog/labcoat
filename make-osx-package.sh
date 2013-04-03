6f23af015edf4f699658775902190e38494ece19
Brian McKenna <brian@precog.com>
2013-04-03 12:03:08 -0600

    Add scripts to generate a node-webkit OS X package

---
+++ make-osx-package.sh
@@ -0,0 +1,17 @@
+APPNAME=Labcoat.app
+NWURL=https://s3.amazonaws.com/node-webkit/v0.4.2/node-webkit-v0.4.2-osx-ia32.zip
+NWZIP=node-webkit.zip
+
+OLDPWD=$PWD
+
+if [ ! -d $APPNAME ]; then
+    curl -o $NWZIP $NWURL
+    unzip $NWZIP
+    rm nwsnapshot
+    mv node-webkit.app $APPNAME
+    rm $NWZIP
+fi
+
+cd bin/debug/node-webkit
+zip -r $OLDPWD/$APPNAME/Contents/Resources/app.nw *
+
